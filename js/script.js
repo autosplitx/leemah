b53d0ea47b311d96d564e3af49446eda578c6071
Abdul <71081071+autosplitx@users.noreply.github.com>
2021-06-23 17:53:09 -0700

    Update Shopping Cart and Minify JS
    Ongoing work on shopping cart and minify the JS file

--- js/script.js
+++ js/script.js
@@ -1,42 +1,63 @@
 window.addEventListener("scroll", () => {
-	let topIcon = document.querySelector(".top-icon");
-	if (topIcon) {
-		const windowScrolled = window.scrollY > 350;
-		topIcon.classList.toggle("scrolling-active", windowScrolled);
-		if (windowScrolled) {
-			let logo = document.querySelector(".top-icon .home");
-			logo.classList.toggle("d-home", window.scrollY > 400);
-		}
-	}
+  let topIcon = document.querySelector(".top-icon");
+  let backToTop = document.querySelector(".back-to-top");
+  if (topIcon) {
+    const windowScrolled = window.scrollY > 350;
+    topIcon.classList.toggle("scrolling-active", windowScrolled);
+    if (windowScrolled) {
+      let logo = document.querySelector(".top-icon .home");
+      logo.classList.toggle("d-home", window.scrollY > 400);
+    }
+  }
+  if (backToTop) {
+    const windowScrolledToTop = window.scrollY > 600;
+    if (windowScrolledToTop) {
+      backToTop.classList.toggle("d-block", window.scrollY > 650);
+    }
+  }
 });
 
 const navLinks = $("#navLinks");
 const filterLinks = $("#filterLinks");
-
 $(".closed").click(function () {
-	if (this.parentElement.className == "logo flex" || this.parentElement.id == "navLinks") {
-		navLinks.toggleClass("open");
-	} else {
-		filterLinks.toggleClass("open-filter");
-	}
+  if (
+    this.parentElement.className == "logo flex" ||
+    this.parentElement.id == "navLinks"
+  ) {
+    navLinks.toggleClass("open");
+  } else {
+    filterLinks.toggleClass("open-filter");
+  }
 });
 
 const tabs = document.querySelectorAll("[data-tab-target]");
 const tabContents = document.querySelectorAll("[data-tab-content]");
 
 tabs.forEach((tab) => {
-	tab.addEventListener("click", () => {
-		const target = document.querySelector(tab.dataset.tabTarget);
-		tabContents.forEach((tabContent) => {
-			tabContent.classList.remove("active");
-		});
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
 
-		tabs.forEach((tab) => {
-			tab.classList.remove("active");
-		});
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
 
-		tab.classList.add("active");
-		target.classList.add("active");
-	});
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
 });
 
+const prodOptCont = document.querySelector('#prod-opt-cont');
+const pocBtn = document.querySelectorAll('.open-btn')
+pocBtn.forEach(el => {
+  el.addEventListener('click', () => {
+    prodOptCont.style.display = 'block'
+  })
+});
+window.addEventListener("mouseup", (e) => {
+  if (e.target != prodOptCont && e.target.parentNode == prodOptCont) {
+    prodOptCont.style.display = "none";
+  }
+});
